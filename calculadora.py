3db3e4e8e156bc7aa21fef7f53907bde389ac92e
wagbrito <wagnerbritodejesus@gmail.com>
2022-12-30 14:49:35 -0300

    Nova versão Refiz todo o código, criando def para cada operação. Só que agora falta descobrir como fazer o loop para e encerrar o programa de vez.

--- calculadora.py
+++ calculadora.py
@@ -1,21 +1,4 @@
-#tela inicial
-print('Olá, escolha uma das opções abaixo.')
-
-print()
-
-print('0 : Soma')
-print('1 : Subtração')
-print('2 : Multiplicação')
-print('3 : Divisão')
-print('4 : Exponenciação')
-
-print()
-
-operacao = int(input('Operação escolhida: '))
-print()
-
-#Soma
-if operacao == 0:
+def soma():
     print('*** A operação escolhida foi Soma (+) ***')
     print()
     n1 = float(input('Digite o primeiro valor: '))
@@ -24,8 +7,7 @@ if operacao == 0:
     print()
     print('O valor da soma é {:.1f}'.format(n))
 
-#Subtração
-if operacao == 1:
+def subtracao():
     print('*** A operação escolhida foi Subtração (-) ***')
     print()
     n1 = float(input('Digite o primeiro valor: '))
@@ -34,8 +16,7 @@ if operacao == 1:
     print()
     print('O valor da subtração é {:.1f}'.format(n))
 
-#Multiplicação
-if operacao == 2:
+def multiplicacao():
     print('*** A operação escolhida foi Multiplicação (*) ***')
     print()
     n1 = float(input('Digite o primeiro valor: '))
@@ -44,8 +25,7 @@ if operacao == 2:
     print()
     print('O valor da multiplicação é {:.1f}'.format(n))
 
-#Divisão
-if operacao == 3:
+def divisao():
     print('*** A operação escolhida foi Divisão (*) ***')
     print()
     n1 = float(input('Digite o primeiro valor: '))
@@ -54,12 +34,50 @@ if operacao == 3:
     print()
     print('O valor da divisão é {:.1f}'.format(n))
 
-#Exponenciação
-if operacao == 4:
+def exponenciacao():
     print('*** A operação escolhida foi Exponenciação (**) ***')
     print()
     n1 = float(input('Digite o primeiro valor: '))
     n2 = float(input('Digite o segundo valor: '))
     n = n1 ** n2
     print()
-    print('O valor da exponenciação é {:.1f}'.format(n))      
+    print('O valor da exponenciação é {:.1f}'.format(n))   
+
+while True:
+    print('Olá, escolha uma das opções abaixo.')
+    
+    print()
+    
+    print('0 : Soma')
+    print('1 : Subtração')
+    print('2 : Multiplicação')
+    print('3 : Divisão')
+    print('4 : Exponenciação')
+    
+    print()
+    
+    menu = int(input('Operação escolhida: '))
+    
+    print()
+    
+    if menu == 0:
+        soma()
+    elif menu == 1:
+        subtracao()
+    elif menu == 2:
+        multiplicacao()
+    elif menu == 3:
+        divisao()
+    elif menu == 4:
+        exponenciacao()
+    else:
+        print('Valor inválido')
+
+    while True:
+        opcao = input('Deseja realizar outra operação? (S/N)? ').upper()
+        if opcao not in ('S', 'N'):
+            print('Opção inválida, deve ser S ou N')
+        else:
+            break
+        if opcao == 'N':
+            break        
